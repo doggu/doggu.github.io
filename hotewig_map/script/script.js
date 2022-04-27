ae1c3385cb9f0284c602f67f6661fb429cf8cd09
doggu <someoneyoumayknow0@gmail.com>
2022-04-27 05:14:32 -0700

    hotewig concept

---
+++ hotewig_map/script/script.js
@@ -0,0 +1,42 @@
+// what am i doing
+
+function button() {
+    console.log("lmao i just tried to print the website")
+}
+
+
+
+// 'use strict';
+//
+// const e = React.createElement;
+//
+// class LikeButton extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { liked: false };
+//     }
+//
+//     render() {
+//         if (this.state.liked) {
+//             const myelement = React.createElement('iframe', {
+//                 width: '560',
+//                 height: '315',
+//                 src: 'https://www.youtube.com/embed/xuCn8ux2gbs?start=212&autoplay=1',
+//                 title: 'YouTube video player',
+//                 frameborder: '0',
+//                 allow: 'accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+//             }, null)
+//
+//             return myelement;
+//         }
+//
+//         return e(
+//             'button',
+//             { onClick: () => this.setState({ liked: true }) },
+//             'Like'
+//         );
+//     }
+// }
+//
+// const domContainer = document.querySelector('#like_button_container');
+// ReactDOM.render(e(LikeButton), domContainer);
